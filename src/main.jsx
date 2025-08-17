a32c74c2c7f44374a6258a5ff4e204cc3dae9d4a
dylanS <dylansong0811@gmail.com>
2025-08-16 23:15:05 -0500

    chore: tidy book row review mapping

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,11 @@ import "./index.css";
 // 关键：用命名空间导入，自动兼容不同导出名
 import * as AppStore from "./store/AppStore";
 // 优先使用 AppProvider，其次 AppStoreProvider；都没有就用 Fragment 兜底
-const StoreProvider = AppStore.AppProvider || AppStore.AppStoreProvider || React.Fragment;
+let StoreProvider = AppStore.AppProvider;
+if (!StoreProvider && "AppStoreProvider" in AppStore) {
+  StoreProvider = AppStore["AppStoreProvider"];
+}
+if (!StoreProvider) StoreProvider = React.Fragment;
 
 const qc = new QueryClient({
   defaultOptions: {
