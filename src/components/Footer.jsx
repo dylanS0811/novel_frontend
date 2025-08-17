25229d60fea1042c302adff9a5ede54e01f3a263
dylanS <dylansong0811@gmail.com>
2025-08-17 18:30:01 -0500

    修改页脚

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 export default function Footer() {
   return (
     <div className="text-center text-xs text-gray-500 py-10">
-      © {new Date().getFullYear()} 硕宝儿倾心制作
+      © {new Date().getFullYear()} 个人制作。保留所有权利。
     </div>
   );
 }
